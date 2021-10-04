76cf5c384681a0f7cc375a23d49ce03cad731bb4
lovejaeuk <lovejaeuk@gmail.co>
2021-10-04 09:19:20 +0900

    Build수정

--- babylonjs-multiplayer-starter-main/client/webpack.prod.js
+++ babylonjs-multiplayer-starter-main/client/webpack.prod.js
@@ -1,7 +1,21 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-maps'
+    mode: 'production',
+    //   devtool: 'source-map',
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "public",
+                    globOptions: {
+                        dot: false,
+                        gitignore: true,
+                        ignore: ["index.html"],
+                    }
+                }
+            ]
+        })]
 });
