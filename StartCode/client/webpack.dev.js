aa28e48b9fe7c0bd0265917fcbad247551911781
lovejaeuk <lovejaeuk@gmail.co>
2021-10-04 10:29:18 +0900

    StartCode

---
+++ StartCode/client/webpack.dev.js
@@ -0,0 +1,23 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+const fs = require('fs');
+
+// App directory
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.resolve(appDirectory, "public"),
+        compress: false,
+        hot: false,
+        liveReload: true,
+        //        publicPath: '/',
+        watchFiles: ['src/**/*', 'public/**/*'],
+        open: true,
+        // host: '0.0.0.0', // enable to access from other devices on the network
+        // https: true // enable when HTTPS is needed (like in WebXR)
+    },
+});
